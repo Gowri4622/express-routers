348b2799e822fef26af41fc8e73a86dc96600ff7
Gowri4622 <gowrihifi@gmail.com>
2023-05-08 09:45:07 +0530

    Changes done in branch page.

--- branchPage.js
+++ branchPage.js
@@ -2,7 +2,7 @@ const express=require('express')
 const router = express.Router()
 
 router.get('/',(request,response)=>{
-    response.send("This is the branch Page")
+    response.send("This is the Branch Page")
 })
 
 module.exports=router
