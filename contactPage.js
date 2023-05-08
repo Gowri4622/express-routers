6ab3e89d1159bf9f2fc989e2af2ef348bdd62927
Gowri4622 <gowrihifi@gmail.com>
2023-05-08 09:45:32 +0530

    Changes done in contact page.

--- contactPage.js
+++ contactPage.js
@@ -2,7 +2,7 @@ const express=require('express')
 const router = express.Router()
 
 router.get('/',(request,response)=>{
-    response.send("This is the contact Page")
+    response.send("This is the Contact Page")
 })
 
 module.exports=router
